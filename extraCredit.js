e4fb568df5a6de5b70f61076734ea9f86c795d62
Latisha Bridges <msbridges3368@gmail.com>
2023-04-14 17:54:36 -0400

    first commit

---
+++ extraCredit.js
@@ -0,0 +1,31 @@
+/**Identify 3 software or technology companies the interest you.
+
+
+ * Google: Google is one of the world's largest technology companies, known for its search engine, email service, and various other products. Google's technology stack includes Java, C++, Python, JavaScript, and more. The company also heavily relies on open source technologies such as Linux, Kubernetes, and TensorFlow. Google's technology choices are driven by its focus on scalability, performance, and developer productivity.
+
+Amazon: Amazon is a leading e-commerce and cloud computing company that offers a wide range of products and services. Amazon's technology stack includes Java, Python, Ruby, and JavaScript, as well as various Amazon-specific technologies such as AWS Lambda and DynamoDB. The company's technology choices are driven by its focus on scalability, reliability, and customer satisfaction.
+
+Netflix: Netflix is a popular streaming video service that offers a vast library of movies and TV shows. Netflix's technology stack includes Java, JavaScript, and various open source technologies such as Apache Kafka and Hadoop. The company's technology choices are driven by its focus on scalability, fault tolerance, and innovation.
+ */
+
+console.log('\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\')
+/**Conduct research to figure out some or all of the elements of each company’s technology stack. Check job openings/job descriptions, engineering blogs, and anything else that may have information about what the company uses to write/build it’s software products.
+
+Google:
+
+Programming languages: Google's technology stack includes a variety of programming languages, including Java, Python, C++, Go, and JavaScript. These languages are used to build various products and services such as Google Search, Google Maps, and Google Cloud.
+Frameworks and tools: Google also relies on a variety of frameworks and tools such as AngularJS, TensorFlow, Kubernetes, and Google Cloud Platform. These tools and frameworks help developers build scalable and efficient applications that can run on Google's infrastructure.
+Open source technologies: Google is a big contributor to open source technologies such as the Linux kernel, Android, and Chromium. Many of Google's own products and services are built on top of these open source technologies.
+Why these technologies: Google's technology choices are driven by its focus on scalability, performance, and developer productivity. The company invests heavily in building and using open source technologies, which helps it leverage the collective expertise of the developer community.
+Amazon:
+
+Programming languages: Amazon's technology stack includes programming languages such as Java, Python, Ruby, and JavaScript. These languages are used to build various products and services such as Amazon Web Services (AWS), Amazon Prime Video, and Amazon Go.
+Frameworks and tools: Amazon relies on a variety of frameworks and tools such as AWS Lambda, DynamoDB, and Amazon Elastic Compute Cloud (EC2). These tools and frameworks help developers build scalable and efficient applications that can run on Amazon's infrastructure.
+Why these technologies: Amazon's technology choices are driven by its focus on scalability, reliability, and customer satisfaction. The company invests heavily in building and using AWS, which allows it to offer a wide range of cloud computing services to its customers.
+Netflix:
+
+Programming languages: Netflix's technology stack includes programming languages such as Java, JavaScript, and Scala. These languages are used to build various products and services such as the Netflix streaming platform and the Netflix content delivery network.
+Frameworks and tools: Netflix relies on a variety of frameworks and tools such as Apache Kafka, Hadoop, and Spinnaker. These tools and frameworks help developers build scalable and efficient applications that can handle the large amounts of data and traffic that Netflix generates.
+Why these technologies: Netflix's technology choices are driven by its focus on scalability, fault tolerance, and innovation. The company invests heavily in building and using open source technologies, which allows it to leverage the collective expertise of the developer community and stay at the forefront of technology trends.
+ * 
+ */
---
+++ menu.js
@@ -0,0 +1,192 @@
+///////////////////////////////////////////////
+///////////////////MENU.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code that
+    deals with food objects, arrays of objects
+    and filtering those arrays. 
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Create an object called `pizza` that has 6
+    properties: 
+        - name (string)
+        - price (number)
+        - category (string)
+        - popularity (number)
+        - rating (number)
+        - tags (array of strings)
+
+    Make sure that you give your properties values
+    of the correct data type.
+
+    Note: the category is something like appetizer/entree,
+    the popularity is an overall ranking, the
+    rating is an average of all customer ratings, and
+    for the tags, think of things that a
+    user might filter by, like 'gluten-free' or
+    'kids'
+*/
+
+//CODE HERE
+const pizza = {
+    name: 'Margherita',
+    price: 5.99,
+    category: 'Vegan',
+    popularity: 7,
+    rating: 4.3,
+    tags: ['cheese', 'California', 'gluten-free']
+}
+console.log(pizza)
+
+//////////////////PROBLEM 2////////////////////
+/* 
+    Let's print a few values from our pizza object.
+
+    First, log the popularity of pizza. 
+    Use dot notation to access the value.
+*/
+
+//CODE HERE
+console.log(pizza.popularity)
+
+/*
+    Second, log the second tag in your pizza's
+    tags array.
+    Use a combination of dots and brackets to
+    get the value.
+*/
+
+//CODE HERE
+
+console.log(pizza.tags[1])
+/*
+    Third, destructure the price off of the
+    pizza object.
+    
+    Print the value of your new price variable.
+*/
+
+//CODE HERE
+const { price } = pizza
+console.log(price)
+
+/*
+    Fourth, and last, destructure the category
+    property.
+
+    Print the value of your category variable. 
+*/
+
+//CODE HERE
+const { category } = pizza
+console.log(category)
+
+//////////////////PROBLEM 3////////////////////
+/* 
+    Create an array with about 5 objects in it.
+    The objects should mimic the `pizza` object.
+    Call the array `foodArr`.
+
+    Make sure that they have slightly different
+    values for price, popularity, rating, and
+    tags. That way, you'll be able to use this
+    data in some functions that you'll write.
+*/
+
+//CODE HERE
+const foodArr = [
+    { name: "Meatlovers", price: 8.99, popularity: 5.0, rating: 4.8, tags: ["all-meat", "American"] },
+    { name: "Veggie Burger", price: 7.99, popularity: 3.8, rating: 4.3, tags: ["vegetarian", "American"] },
+    { name: "Chicken Caesar Salad", price: 9.99, popularity: 4.0, rating: 4.6, tags: ["salad", "chicken"] },
+    { name: "Pesto Pasta", price: 10.99, popularity: 3.5, rating: 4.2, tags: ["pasta", "italian"] }
+  ]
+
+
+//////////////////PROBLEM 4////////////////////
+/* 
+    Let's filter the food objects according
+    to their tags.
+
+    Write a callback function below that will
+    return only food objects that have a certain tag.
+
+    You can check for any tag that at least 1 of
+    your food objects has.
+*/
+
+//CODE HERE
+function filterByTag(tag) {
+    return foodArr.filter(food => food.tags.includes(tag))
+    }
+
+
+const filteredFood = filterByTag('American')
+console.log(filteredFood)
+
+
+//////////////////PROBLEM 5////////////////////
+/* 
+    Now let's write a function that's a little
+    more flexible than just filtering for one
+    value. We want to be able to filter for 
+    food that has above a certain rating, 
+    below a certain price, or any other combo.
+
+    Write a function called `filterByProperty`
+    that takes in three arguments: `property`, 
+    `number`, and `type. 
+
+    The property will be a string (rating,
+    popularity, or price)
+
+    The number will be the number that you want
+    to compare against 
+
+    The type should be 'above' or 'below' to 
+    indicate whether you want to get foods with
+    values that are above or below the given number
+    for the given property
+
+    Inside the function, create a variable to hold
+    a filtered array
+
+    Use the filter method to filter the foodArr
+
+        In the callback, check if the `type` is `above`, 
+        if it is, return objects whose value for the given
+        property is greater than the `number` passed in
+
+        If the type isn't `below`, return objects whose
+        value for the given property is less than the 
+        `number` passed in
+    
+    Return the filtered array from the entire function
+*/
+
+//CODE HERE
+function filterByProperty(property, number, type) {
+    let filteredArray = []
+  
+    if (type === "above") {
+      filteredArray = foodArr.filter((food) => food[property] > number)
+    } else if (type === "below") {
+      filteredArray = foodArr.filter((food) => food[property] < number)
+    }
+  
+    return filteredArray
+  }
+
+
+/*
+    Invoke the `filterByProperty` function passing
+    in a value for each paramter.
+
+    You'll have to console.log to see the filtered array
+*/
+
+//CODE HERE
+const filteredFoods = filterByProperty("rating", 4.5, "above");
+console.log(filteredFoods);
